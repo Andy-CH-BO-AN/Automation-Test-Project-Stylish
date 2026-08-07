8b4d50b9bc753343b3003c7d7a230918eb074efc
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:11:53 +0800

    avoid mutating session headers in order api

--- api_objects/create_order_api.py
+++ api_objects/create_order_api.py
@@ -1,18 +1,10 @@
-import os
 from utils.api_utils import ApiBase
 
 
 class CreateOrderApi(ApiBase):
     def __init__(self, session, payload):
-        self.base_url = os.getenv("BASE_URL")
-        url = f"{self.base_url}/order"
-        super().__init__(session, url)
-        self.session = session
+        super().__init__(session, "/order")
         self.payload = payload
-        self.session.headers = {
-            "Authorization": self.session.headers["Authorization"]
-        }
 
     def send(self):
-        response = self.api_request("post", self.payload)
-        return response
+        return self.api_request("post", payload=self.payload)
