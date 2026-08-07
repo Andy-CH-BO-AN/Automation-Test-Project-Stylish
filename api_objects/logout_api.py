2943746dfff23cbf14a0dc460e67bf7a1a81b9e5
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:11:45 +0800

    centralize logout api url construction

--- api_objects/logout_api.py
+++ api_objects/logout_api.py
@@ -1,14 +1,9 @@
-import os
 from utils.api_utils import ApiBase
 
 
 class LogoutApi(ApiBase):
     def __init__(self, session):
-        self.base_url = os.getenv("BASE_URL")
-        url = f"{self.base_url}/user/logout"
-        super().__init__(session, url)
-        self.session = session
+        super().__init__(session, "/user/logout")
 
     def send(self):
-        response = self.api_request("post")
-        return response
+        return self.api_request("post")
