c6be0013e5c1a3cb8c644d4d77033712a8d546dc
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:01:23 +0800

    refactor: make API fixtures xdist optional

--- tests_api/conftest.py
+++ tests_api/conftest.py
@@ -1,11 +1,11 @@
 import logging
-import os
 
 import pytest
 import requests
 
 from api_objects.login_api import LoginApi
-from api_objects.logout_api import LogoutApi
+from utils.test_credentials import get_worker_credentials
+
 
 @pytest.fixture()
 def session():
@@ -16,19 +16,9 @@ def session():
 
 @pytest.fixture()
 def login(request, session):
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.INFO)
+    email, password = get_worker_credentials(request)
 
-    worker_id = request.config.workerinput['workerid']
-    if worker_id == 'gw0':
-        email = os.getenv('USER_NAME_1')
-        password = os.getenv('PASSWORD')
-    elif worker_id == 'gw1':
-        email = os.getenv('USER_NAME_2')
-        password = os.getenv('PASSWORD')
-    else:
-        email = os.getenv('USER_NAME_1')
-        password = os.getenv('PASSWORD')
     login_api = LoginApi(session=session, email=email, password=password)
     response = login_api.send()
     yield response, session
