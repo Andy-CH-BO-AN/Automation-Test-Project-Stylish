4a630beb734f29c4a5a07301634a972176d1a348
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:02:11 +0800

    refactor: parameterize user queries

--- table_object/user_table.py
+++ table_object/user_table.py
@@ -2,6 +2,6 @@ import db_utilis.db_utils as db_utils
 
 
 def search_user_info(email, conn):
-    command = f"SELECT * FROM stylish_backend.user WHERE email = '{email}';"
-    user_info = db_utils.query_method(command, conn)
+    command = "SELECT * FROM stylish_backend.user WHERE email = %s"
+    user_info = db_utils.query_method(command, conn, params=(email,))
     return user_info[0]
