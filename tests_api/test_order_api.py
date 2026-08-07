e1d45e62d42d1e44444bbde6ca53430fa1818a55
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:03:37 +0800

    refactor: isolate order test data

--- tests_api/test_order_api.py
+++ tests_api/test_order_api.py
@@ -1,144 +1,127 @@
 import json
-import logging
 import os
+from copy import deepcopy
 
-import allure
 import pytest
+
 from api_objects.create_order_api import CreateOrderApi
-from api_objects.get_order_prime_api import GetOrderPrimeApi
 from api_objects.get_order_api import GetOrderApi
+from api_objects.get_order_prime_api import GetOrderPrimeApi
 
 
-class TestOrderApi:
-    test_card_num = "4242424242424242"
-    test_card_due_date = "202404"
-    test_card_ccv = "424"
-
-    order_info = {
-        "prime": '',
-
-        "order": {
-            "shipping": "delivery",
-            "payment": "credit_card",
-            "subtotal": 1797,
-            "freight": 30,
-            "total": 1827,
-
-            "recipient": {
-                "name": "mane",
-                "phone": "0809000550",
-                "email": "aaa@bbb.com",
-                "address": "台北市中正區重慶南路一段122號",
-                "time": ""
-            },
-
-            "list": [{
-                "color": {
-                    "code": "FFFFFF",
-                    "name": "白色"
-                },
+ORDER_TEMPLATE = {
+    "prime": "",
+    "order": {
+        "shipping": "delivery",
+        "payment": "credit_card",
+        "subtotal": 1797,
+        "freight": 30,
+        "total": 1827,
+        "recipient": {
+            "name": "mane",
+            "phone": "0809000550",
+            "email": "aaa@bbb.com",
+            "address": "台北市中正區重慶南路一段122號",
+            "time": "",
+        },
+        "list": [
+            {
+                "color": {"code": "FFFFFF", "name": "白色"},
                 "id": "314562783901",
                 "image": "http://abc.com/assets/3456789/main.jpg",
                 "name": "洋裝",
                 "price": 599,
                 "qty": 3,
-                "size": "S"
-            }]
-        }
-    }
+                "size": "S",
+            }
+        ],
+    },
+}
 
-    @pytest.mark.parametrize(argnames="time",
-                             argvalues=["anytime", "morning", "afternoon"])
-    def test_user_can_checkout(self, time, login, session):
-        get_prime_api = GetOrderPrimeApi(session, cardnumber=self.test_card_num, cardduedate=self.test_card_due_date,
-                                         cardccv=self.test_card_ccv)
-        response = get_prime_api.send()
-        prime = json.loads(response.text)["card"]["prime"]
-        self.order_info["prime"] = prime
-        self.order_info["order"]["recipient"]["time"] = time
 
-        create_order_api = CreateOrderApi(session, self.order_info)
-        response = create_order_api.send()
-        order_number = response.json()['data']['number']
+@pytest.fixture(params=["anytime", "morning", "afternoon"])
+def delivery_time(request):
+    return request.param
 
-        get_order_api = GetOrderApi(session, order_number)
-        response = get_order_api.send()
-        order_detail = response.json()
-        order_detail["data"]["number"] = order_number
-        assert self.order_info["order"] == order_detail["data"]["details"], \
-            logging.info(f"self.order_info['order']: {self.order_info['order']} "
-                         f"order_detail['data']['details']: {order_detail['data']['details']}")
 
-    def test_user_cannot_checkout_with_invalid_prime(self, time, login, session):
-        get_prime_api = GetOrderPrimeApi(session, cardnumber=self.test_card_num, cardduedate=self.test_card_due_date,
-                                         cardccv=self.test_card_ccv)
-        response = get_prime_api.send()
-        prime = f"{json.loads(response.text)['card']['prime']}a"
-        order_info = self.order_info
-        order_info["prime"] = prime
-        order_info["order"]["recipient"]["time"] = time
+@pytest.fixture()
+def order_info():
+    return deepcopy(ORDER_TEMPLATE)
 
-        create_order_api = CreateOrderApi(session, order_info)
-        response = create_order_api.send()
+
+def get_prime(session):
+    required_vars = (
+        "TAPPAY_TEST_CARD_NUM",
+        "TAPPAY_TEST_CARD_DUE_DATE",
+        "TAPPAY_TEST_CARD_CCV",
+    )
+    card_data = {name: os.getenv(name) for name in required_vars}
+    missing = [name for name, value in card_data.items() if not value]
+    if missing:
+        pytest.skip(f"Missing TapPay sandbox config: {', '.join(missing)}")
+
+    response = GetOrderPrimeApi(
+        session,
+        cardnumber=card_data["TAPPAY_TEST_CARD_NUM"],
+        cardduedate=card_data["TAPPAY_TEST_CARD_DUE_DATE"],
+        cardccv=card_data["TAPPAY_TEST_CARD_CCV"],
+    ).send()
+    return json.loads(response.text)["card"]["prime"]
+
+
+class TestOrderApi:
+    def test_user_can_checkout(self, delivery_time, order_info, login, session):
+        order_info["prime"] = get_prime(session)
+        order_info["order"]["recipient"]["time"] = delivery_time
+
+        response = CreateOrderApi(session, order_info).send()
+        assert response.status_code == 200
+        order_number = response.json()["data"]["number"]
+
+        response = GetOrderApi(session, order_number).send()
+        assert response.status_code == 200
+        assert order_info["order"] == response.json()["data"]["details"]
+
+    def test_user_cannot_checkout_with_invalid_prime(self, delivery_time, order_info, login, session):
+        order_info["prime"] = f"{get_prime(session)}a"
+        order_info["order"]["recipient"]["time"] = delivery_time
+
+        response = CreateOrderApi(session, order_info).send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Prime is invalid.'
+        assert response.json()["errorMsg"] == "Prime is invalid."
 
-    def test_user_cannot_checkout_without_prime(self, time, login, session):
-        get_prime_api = GetOrderPrimeApi(session, cardnumber=self.test_card_num, cardduedate=self.test_card_due_date,
-                                         cardccv=self.test_card_ccv)
-        get_prime_api.send()
-        self.order_info["order"]["recipient"]["time"] = time
+    def test_user_cannot_checkout_without_prime(self, delivery_time, order_info, login, session):
+        order_info["order"]["recipient"]["time"] = delivery_time
 
-        create_order_api = CreateOrderApi(session, self.order_info)
-        response = create_order_api.send()
+        response = CreateOrderApi(session, order_info).send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Prime is required.'
+        assert response.json()["errorMsg"] == "Prime is required."
 
     def test_user_cannot_checkout_with_invalid_order_info(self, login, session):
-        get_prime_api = GetOrderPrimeApi(session, cardnumber=self.test_card_num, cardduedate=self.test_card_due_date,
-                                         cardccv=self.test_card_ccv)
-        get_prime_api.send()
-        order_info = "OvO"
-
-        create_order_api = CreateOrderApi(session, order_info)
-        response = create_order_api.send()
+        response = CreateOrderApi(session, "OvO").send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Order info is invalid'
+        assert response.json()["errorMsg"] == "Order info is invalid"
 
     def test_user_cannot_checkout_without_order_info(self, login, session):
-        get_prime_api = GetOrderPrimeApi(session, cardnumber=self.test_card_num, cardduedate=self.test_card_due_date,
-                                         cardccv=self.test_card_ccv)
-        get_prime_api.send()
-        order_info = ''
-        create_order_api = CreateOrderApi(session, order_info)
-        response = create_order_api.send()
+        response = CreateOrderApi(session, "").send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Order info is required.'
+        assert response.json()["errorMsg"] == "Order info is required."
 
-    def test_user_cannot_get_order_detail_with_invalid_order_number(self, time, login, session):
-        get_prime_api = GetOrderPrimeApi(session, cardnumber=self.test_card_num, cardduedate=self.test_card_due_date,
-                                         cardccv=self.test_card_ccv)
-        response = get_prime_api.send()
-        prime = json.loads(response.text)["card"]["prime"]
-        self.order_info["prime"] = prime
-        self.order_info["order"]["recipient"]["time"] = time
+    def test_user_cannot_get_order_detail_with_invalid_order_number(
+        self, delivery_time, order_info, login, session
+    ):
+        order_info["prime"] = get_prime(session)
+        order_info["order"]["recipient"]["time"] = delivery_time
 
-        create_order_api = CreateOrderApi(session, self.order_info)
-        response = create_order_api.send()
-        order_number = f"{response.json()['data']['number']}a"
+        response = CreateOrderApi(session, order_info).send()
+        assert response.status_code == 200
+        invalid_order_number = f"{response.json()['data']['number']}a"
 
-        get_order_api = GetOrderApi(session, order_number)
-        response = get_order_api.send()
-        order_detail = response.json()
-        order_detail["data"]["number"] = order_number
+        response = GetOrderApi(session, invalid_order_number).send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Order number is invalid'
+        assert response.json()["errorMsg"] == "Order number is invalid"
 
     def test_user_cannot_get_order_detail_without_order_number(self, login, session):
-        order_number = ""
-        get_order_api = GetOrderApi(session, order_number)
-        response = get_order_api.send()
-        order_detail = response.json()
-        order_detail["data"]["number"] = order_number
+        response = GetOrderApi(session, "").send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Order number is required.'
+        assert response.json()["errorMsg"] == "Order number is required."
