2e87f77afc69409f58fe4bd92a294fe58b5a2f69
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:15:07 +0800

    simplify create and delete product tests

--- tests_api/test_create_and_delete_product_api.py
+++ tests_api/test_create_and_delete_product_api.py
@@ -1,234 +1,117 @@
-from table_object.color_table import search_color
-from test_data import test_data_from_excel
+import requests
 import allure
 import pytest
+
 from api_objects.create_product_api import CreateProductApi
 from api_objects.delete_product_api import DeleteProductApi
-from api_objects.products_search_api import ProductsSearchApi
 from api_objects.products_detail_api import ProductsDetailApi
-from api_objects.logout_api import LogoutApi
+from test_data import test_data_from_excel
+from tests_api.product_test_helpers import (
+    delete_product_if_exists,
+    find_product_by_title,
+    product_id,
+    product_payload_from_row,
+)
+
+
+def sample_product_payload():
+    return {
+        "category": "men",
+        "title": "m",
+        "description": "m",
+        "price": 69,
+        "texture": "m",
+        "wash": "m",
+        "place": "m",
+        "note": "m",
+        "color_ids": ["1", "2"],
+        "sizes": ["M", "XL"],
+        "story": "m",
+        "main_image": "mainImage.jpg",
+        "other_images": ["otherImage0.jpg", "otherImage1.jpg"],
+    }
 
 
 @allure.feature("Create and delete product")
 class TestCreateProduct:
-    @pytest.mark.parametrize('product_info',
-                             test_data_from_excel.read_data('test_data/Stylish_TestCase.xlsx',
-                                                            'API Create Product Success'))
+    @pytest.mark.parametrize(
+        "product_row",
+        test_data_from_excel.read_data(
+            "test_data/Stylish_TestCase.xlsx",
+            "API Create Product Success",
+        ),
+    )
     @allure.title("admin can create and delete product success")
-    def test_admin_can_create_and_delete_product(self, login, session, product_info, setup_db, request):
-        keyword = 'm'
-        paging = 0
+    def test_admin_can_create_and_delete_product(self, login, session, product_row):
+        payload = product_payload_from_row(product_row)
 
-        product_info = {
-            'category': product_info['Category'],
-            'title': product_info['Title'],
-            'description': product_info['Description'],
-            'price': product_info['Price'],
-            'texture': product_info['Texture'],
-            'wash': product_info['Wash'],
-            'place': product_info['Place of Product'],
-            'note': product_info['Note'],
-            'color_ids': product_info['ColorIDs'],
-            'sizes': product_info['Sizes'],
-            'story': product_info['Story'],
-            'main_image': product_info['Main Image'],
-            'other_images': [product_info['Other Image 1'], product_info['Other Image 2']]
-        }
-        create_product_api = CreateProductApi(session, product_info)
-        response = create_product_api.send()
-        assert response.status_code == 200
+        try:
+            response = CreateProductApi(session, payload).send()
+            assert response.status_code == 200
 
-        color_ids = product_info['ColorIDs'].split(",")
-        for color in range(len(color_ids)):
-            color_ids[color] = search_color(color_ids[color], setup_db)
+            product = find_product_by_title(session, payload["title"])
+            assert product is not None
 
-        sizes = product_info['sizes'].split(",")
+            created_product_id = product_id(product)
+            response = DeleteProductApi(session, created_product_id).send()
+            assert response.status_code == 200
 
-        product_search_api = ProductsSearchApi(session, keyword, paging)
-        response = product_search_api.send()
-        product_detail_data = response.json()[0]["data"]
+            response = ProductsDetailApi(session, created_product_id).send()
+            assert response.status_code == 404
+        finally:
+            delete_product_if_exists(session, payload["title"])
 
-        product_detail_colors = []
-        product_detail_sizes = []
+    @pytest.mark.parametrize(
+        "product_row",
+        test_data_from_excel.read_data(
+            "test_data/Stylish_TestCase.xlsx",
+            "API Create Product Failed",
+        ),
+    )
+    @allure.title("admin cannot create product with invalid value")
+    def test_admin_cannot_create_product_with_invalid_value(self, login, session, product_row):
+        payload = product_payload_from_row(product_row)
+        try:
+            response = CreateProductApi(session, payload).send()
+            assert response.status_code == 400
+            assert response.json()["errorMsg"] == product_row["Error Msg"]
+        finally:
+            delete_product_if_exists(session, payload["title"])
 
-        assert product_detail_data['Category'] == product_info['Category']
-        assert product_detail_data['title'] == product_info['Title']
-        assert product_detail_data['Description'] == product_info['Description']
-        assert product_detail_data['Price'] == product_info['Price']
-        assert product_detail_data['Texture'] == product_info['Texture']
-        assert product_detail_data['Wash'] == product_info['Wash']
-        assert product_detail_data['Place of Product'] == product_info['Place of Product']
-        assert product_detail_data['Note'] == product_info['Note']
-        assert product_detail_data['main_image'] == product_info['main_image']
-        assert product_detail_data['other_images'] == product_info['other_images']
-        assert product_detail_data['story'] == product_info['story']
-
-        for variant in product_detail_data['variants']:
-            if variant["color_code"] not in product_detail_colors:
-                product_detail_colors.append(variant["color_code"])
-        assert product_detail_colors == color_ids
-
-        for variant in product_detail_data['variants']:
-            if variant["size"] not in product_detail_sizes:
-                product_detail_sizes.append(variant["size"])
-        assert product_detail_sizes == sizes
-
-        product_id = product_detail_data['product_id']
-        delete_product_api = DeleteProductApi(session, product_id)
-        response = delete_product_api.send()
-
-        assert response.status_code == 200
-
-        product_search_api = ProductsDetailApi(session, product_id)
-        response = product_search_api.send()
-
-        assert response.status_code == 404
-
-        def teardown_method():
-            product_search_api = ProductsSearchApi(session, keyword, paging)
-            response = product_search_api.send()
-            if len(response.json()) > 0:
-                product_detail_data = response.json()[0]["data"]
-                product_id = product_detail_data['product_id']
-                delete_product_api = DeleteProductApi(session, product_id)
-                delete_product_api.send()
-        request.addfinalizer(teardown_method)
-
-    @pytest.mark.parametrize('product_info',
-                             test_data_from_excel.read_data('test_data/Stylish_TestCase.xlsx',
-                                                            'API Create Product Failed'))
-    def test_admin_cannot_create_product_with_invalid_value(self, login, session, product_info, request):
-        self.product_info = product_info
-        product_info = {
-            'category': product_info['Category'],
-            'title': product_info['Title'],
-            'description': product_info['Description'],
-            'price': product_info['Price'],
-            'texture': product_info['Texture'],
-            'wash': product_info['Wash'],
-            'place': product_info['Place of Product'],
-            'note': product_info['Note'],
-            'color_ids': product_info['ColorIDs'],
-            'sizes': product_info['Sizes'],
-            'story': product_info['Story'],
-            'main_image': product_info['Main Image'],
-            'other_images': [product_info['Other Image 1'], product_info['Other Image 2']]
-        }
-        create_product_api = CreateProductApi(session, product_info)
-        response = create_product_api.send()
-        assert response.status_code == 400
-        assert response.json()['errorMsg'] == self.product_info['Error Msg']
-
-        def teardown_method():
-            keyword = 'm'
-            paging = 0
-            product_search_api = ProductsSearchApi(session, keyword, paging)
-            response = product_search_api.send()
-            if len(response.json()) > 0:
-                product_detail_data = response.json()[0]["data"]
-                product_id = product_detail_data['product_id']
-                delete_product_api = DeleteProductApi(session, product_id)
-                delete_product_api.send()
-        request.addfinalizer(teardown_method)
-
-    def test_admin_cannot_create_product_without_login(self, session, request):
-        product_info = {
-            'category': 'men',
-            'title': 'm',
-            'description': 'm',
-            'price': 69,
-            'texture': 'm',
-            'wash': 'm',
-            'place': 'm',
-            'note': 'm',
-            'color_ids': ['1', '2'],
-            'sizes': ['M', 'XL'],
-            'story': 'm',
-            'main_image': 'mainImage.jpg',
-            'other_images': ['otherImage0.jpg', 'otherImage1.jpg']
-        }
-        create_product_api = CreateProductApi(session, product_info)
-        response = create_product_api.send()
+    @allure.title("admin cannot create product without login")
+    def test_admin_cannot_create_product_without_login(self, session):
+        response = CreateProductApi(session, sample_product_payload()).send()
         assert response.status_code == 401
-        assert response.json()['errorMsg'] == 'Unauthorized'
+        assert response.json()["errorMsg"] == "Unauthorized"
 
-        def teardown_method():
-            keyword = 'm'
-            paging = 0
-            product_search_api = ProductsSearchApi(session, keyword, paging)
-            response = product_search_api.send()
-            if len(response.json()) > 0:
-                product_detail_data = response.json()[0]["data"]
-                product_id = product_detail_data['product_id']
-                delete_product_api = DeleteProductApi(session, product_id)
-                delete_product_api.send()
-        request.addfinalizer(teardown_method)
+    @allure.title("admin cannot delete product without login")
+    def test_admin_cannot_delete_product_without_login(self, login, session):
+        payload = sample_product_payload()
+        try:
+            response = CreateProductApi(session, payload).send()
+            assert response.status_code == 200
 
-    def test_admin_cannot_delete_product_without_login(self, login, session, setup_db, request):
-        keyword = 'm'
-        paging = 0
+            product = find_product_by_title(session, payload["title"])
+            assert product is not None
 
-        product_info = {
-            'category': 'men',
-            'title': 'm',
-            'description': 'm',
-            'price': 69,
-            'texture': 'm',
-            'wash': 'm',
-            'place': 'm',
-            'note': 'm',
-            'color_ids': ['1', '2'],
-            'sizes': ['M', 'XL'],
-            'story': 'm',
-            'main_image': 'mainImage.jpg',
-            'other_images': ['otherImage0.jpg', 'otherImage1.jpg']
-        }
-        create_product_api = CreateProductApi(session, product_info)
-        response = create_product_api.send()
-        assert response.status_code == 200
-
-        product_search_api = ProductsSearchApi(session, keyword, paging)
-        response = product_search_api.send()
-        product_detail_data = response.json()[0]["data"]
-        product_id = product_detail_data['product_id']
-
-        logout_api = LogoutApi(session)
-        logout_api.send()
-
-        delete_product_api = DeleteProductApi(session, product_id)
-        response = delete_product_api.send()
-        assert response.status_code == 401
-        assert response.json()['errorMsg'] == 'Unauthorized'
-
-        def teardown_method():
-            keyword = 'm'
-            paging = 0
-            product_search_api = ProductsSearchApi(session, keyword, paging)
-            response = product_search_api.send()
-            if len(response.json()) > 0:
-                product_detail_data = response.json()[0]["data"]
-                product_id = product_detail_data['product_id']
-                delete_product_api = DeleteProductApi(session, product_id)
-                delete_product_api.send()
-        request.addfinalizer(teardown_method)
+            with requests.Session() as anonymous_session:
+                response = DeleteProductApi(anonymous_session, product_id(product)).send()
+            assert response.status_code == 401
+            assert response.json()["errorMsg"] == "Unauthorized"
+        finally:
+            delete_product_if_exists(session, payload["title"])
 
     def test_admin_cannot_delete_product_without_product_id(self, login, session):
-        product_id = ""
-        delete_product_api = DeleteProductApi(session, product_id)
-        response = delete_product_api.send()
+        response = DeleteProductApi(session, "").send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Product ID not found.'
+        assert response.json()["errorMsg"] == "Product ID not found."
 
     def test_admin_cannot_delete_product_with_product_id_not_exist(self, login, session):
-        product_id = 123412341234
-        delete_product_api = DeleteProductApi(session, product_id)
-        response = delete_product_api.send()
+        response = DeleteProductApi(session, 123412341234).send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Product ID not found.'
+        assert response.json()["errorMsg"] == "Product ID not found."
 
     def test_admin_cannot_delete_product_with_invalid_product_id(self, login, session):
-        product_id = "a"
-        delete_product_api = DeleteProductApi(session, product_id)
-        response = delete_product_api.send()
+        response = DeleteProductApi(session, "a").send()
         assert response.status_code == 400
-        assert response.json()['errorMsg'] == 'Invalid Product ID'
+        assert response.json()["errorMsg"] == "Invalid Product ID"
