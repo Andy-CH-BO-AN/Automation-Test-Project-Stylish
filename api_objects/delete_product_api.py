64c75e48e568b533efccf88783587c1afd071c11
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2026-08-08 01:12:39 +0800

    centralize delete product api url construction

--- api_objects/delete_product_api.py
+++ api_objects/delete_product_api.py
@@ -1,14 +1,9 @@
-import os
 from utils.api_utils import ApiBase
 
 
 class DeleteProductApi(ApiBase):
     def __init__(self, session, product_id):
-        self.base_url = os.getenv("BASE_URL")
-        self.product_id = product_id
-        url = f"{self.base_url}/admin/{product_id}"
-        super().__init__(session, url)
+        super().__init__(session, f"/admin/{product_id}")
 
     def send(self):
-        response = self.api_request("delete")
-        return response
+        return self.api_request("delete")
